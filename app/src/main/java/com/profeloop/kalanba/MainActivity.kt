a05b90b5741acab8912dbb48f803cef3a480ab7d
kraqinc <kraqai.c@gmail.com>
2026-05-06 19:05:50 -0500

    fix: install splash screen in MainActivity

--- app/src/main/java/com/profeloop/kalanba/MainActivity.kt
+++ app/src/main/java/com/profeloop/kalanba/MainActivity.kt
@@ -2,6 +2,7 @@ package com.profeloop.kalanba
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
@@ -28,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     var currentUser: User? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         // Check if user is logged in
